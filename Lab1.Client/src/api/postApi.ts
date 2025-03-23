3d980bb4b9ea8599c9ed1fd865d5d6eeebe6ba94
adaspera <jusciokas1@gmail.com>
2025-03-23 18:00:49 +0200

    .

--- Lab1.Client/src/api/postApi.ts
+++ Lab1.Client/src/api/postApi.ts
@@ -19,6 +19,12 @@ export interface CreatePostDto {
     topicIds: number[];
 }
 
+export interface UpdatePostDto {
+    id: number;
+    title: string;
+    topicIds: number[];
+}
+
 export const fetchPosts = async (): Promise<Post[]> => {
     const response = await axiosInstance.get<Post[]>('/posts');
 
@@ -79,4 +85,20 @@ export const createPost = async (data: CreatePostDto): Promise<Post> => {
 export const deletePost = async (id: number) => {
     const response = await axiosInstance.delete<Post>(`/posts/${id}`);
     return response;
+};
+
+export const updatePost = async (data: UpdatePostDto) => {
+    const response = await axiosInstance.put<Post>(`/posts`, data);
+    const post = response.data;
+
+    if (post.imageData) {
+        const base64 = btoa(
+            new Uint8Array(post.imageData).reduce(
+                (data, byte) => data + String.fromCharCode(byte), ''
+            )
+        );
+        post.imageBase64 = `data:${post.imageType};base64,${base64}`;
+    }
+
+    return post;
 };
